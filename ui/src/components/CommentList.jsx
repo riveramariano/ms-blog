88c50d130b1001ab5e39c7fce827c69ffbe40ffe
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 16:44:03 -0600

    Update Comments Content
    Now the comments content will change conditionaly of it's status.

--- ui/src/components/CommentList.jsx
+++ ui/src/components/CommentList.jsx
@@ -2,7 +2,24 @@ import React from "react";
 
 const CommentList = ({ comments }) => {
   const rederedComments = comments.map((comment) => {
-    return <li key={comment.id}>{comment.content}</li>;
+    let content = '';
+
+    switch (comment.status) {
+      case 'approved':
+        content = comment.content;
+        break;
+      case 'rejected':
+        content = "This comment has been rejected";
+        break;
+      case 'pending':
+        content = 'This comment is awaiting moderation';
+        break;
+      default:
+        content = 'An error has happened';
+        break;
+    }
+
+    return <li key={comment.id}>{content}</li>;
   });
 
   return <ul>{rederedComments}</ul>;
