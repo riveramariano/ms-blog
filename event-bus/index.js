bf483923677a4dba521ac1e948244ddc103b75fe
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 17:18:10 -0600

    Event Syncing
    Fix the problem where data is lost if a microservice goes down.

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,16 +5,38 @@ const axios = require('axios');
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post('/events', (req, res) => {
   const event = req.body;
 
+  /* 
+    In case a microservice will posibly be interrupted we store
+    all the events data, so when the microservice is running again we send 
+    the data to it, making posible something like changing the status in 
+    the case it was the moderation service.
+  */
+  events.push(event);
+
   // Sends the event to each microservice
-  axios.post('http://localhost:4000/events', event); // Posts
-  axios.post('http://localhost:4001/events', event); // Comments
-  axios.post('http://localhost:4002/events', event); // Query
-  axios.post('http://localhost:4003/events', event); // Moderation
+  axios.post('http://localhost:4000/events', event).catch((err) => {
+    console.log(err.message);
+  }); // Posts
+  axios.post('http://localhost:4001/events', event).catch((err) => {
+    console.log(err.message);
+  }); // Comments
+  axios.post('http://localhost:4002/events', event).catch((err) => {
+    console.log(err.message);
+  }); // Query
+  axios.post('http://localhost:4003/events', event).catch((err) => {
+    console.log(err.message);
+  }); // Moderation
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (_req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
--- query/index.js
+++ query/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -8,13 +9,7 @@ app.use(cors());
 
 const posts = {};
 
-app.get('/posts', (_req, res) => {
-  res.send(posts);
-});
-
-app.post('/events', (req, res) => {
-  const { type, data } = req.body;
-  
+const handleEvent = (type, data) => {
   if (type === 'PostCreated') {
     const { id, title } = data;
     posts[id] = { id, title, comments: [] };
@@ -37,10 +32,29 @@ app.post('/events', (req, res) => {
     comment.content = content;
     comment.status = status;
   }
+};
+
+app.get('/posts', (_req, res) => {
+  res.send(posts);
+});
+
+app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  handleEvent(type, data);
 
   res.send({});
 });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
   console.log('Listening on 4002');
+
+  try {
+    const res = await axios.get('http://localhost:4005/events');
+    for (const event of res.data) {
+      console.log('Processing Event:', event.type);
+      handleEvent(event.type, event.data);
+    }
+  } catch (error) {
+    console.log(error.message);
+  }
 });
