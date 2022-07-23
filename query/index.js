7322431c2472b5c5c4d909bdd76ce1e2d08b0f7d
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 15:27:56 -0600

    Query Service Finished
    Add the logic for query service to be used by the frontend.

--- query/index.js
+++ query/index.js
@@ -6,12 +6,27 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {
+const posts = {};
 
+app.get('/posts', (_req, res) => {
+  res.send(posts);
 });
 
 app.post('/events', (req, res) => {
+  const { type, data } = req.body;
+  
+  if (type === 'PostCreated') {
+    const { id, title } = data;
+    posts[id] = { id, title, comments: [] };
+  }
 
+  if (type === 'CommentCreated') {
+    const { id, content, postId } = data;
+    const post = posts[postId];
+    post.comments.push({ id, content });
+  }
+
+  res.send({});
 });
 
 app.listen(4002, () => {
