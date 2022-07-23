3286c1e435a52f0af07fad8214f7457031694a78
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 12:30:10 -0600

    Listing Posts
    Add cards to each post and fix index.js render problem.

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,7 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
