87ac5c09193b92d8c03589e7e31b35fb9c0fa317
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 11:31:21 -0600

    Comments Service
    Create basic logic for comments service.

---
+++ comments/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { randomBytes } = require('crypto');
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentsByPostId = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+  res.send(commentsByPostId[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+  const commentId = randomBytes(4).toString('hex');
+  const { content } = req.body;
+
+  // If there aren't any comments initialize empty array
+  const comments = commentsByPostId[req.params.id] || [];
+  comments.push({ id: commentId, content });
+
+  commentsByPostId[req.params.id] = comments;
+  res.status(201).send(comments);
+});
+
+app.listen(4002, () => {
+  console.log('Listening on 4002');
+});
