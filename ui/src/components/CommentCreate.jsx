db0aeee7129b99fcfa7737941bd169081f191ca3
Mariano Rivera <rivveramariano@gmail.com>
2022-07-23 12:40:15 -0600

    Add Comment
    Add the ability to create new comments to specific posts.

---
+++ ui/src/components/CommentCreate.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import Axios from 'axios';
+
+const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    await Axios.post(`http://localhost:4002/posts/${postId}/comments`, { content });
+    setContent('');
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label>New Comment</label>
+          <input className="form-control" value={content} onChange={e => setContent(e.target.value)} />
+        </div>
+        <button className="btn btn-primary mt-3">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default CommentCreate;
--- ui/src/components/PostList.jsx
+++ ui/src/components/PostList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Axios from 'axios';
+import CommentCreate from "./CommentCreate";
 
 const PostList = () => {
   const [posts, setPosts] = useState({});
@@ -18,6 +19,7 @@ const PostList = () => {
       <div className="card" style={{ width: '30%', marginBottom: '20px' }} key={post.id}>
         <div className="card-body">
           <h3>{post.title}</h3>
+          <CommentCreate postId={post.id} />
         </div>
       </div>
     );
